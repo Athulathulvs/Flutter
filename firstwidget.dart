312d1ac88bc6e433877b6d14b95be6d530ce3857
Athulathulvs <97433492+Athulathulvs@users.noreply.github.com>
2022-08-08 22:38:26 +0530

    Add files via upload

---
+++ firstwidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  MaterialApp app = MaterialApp(
+    title: "NEW PROJECT",
+    home: Scaffold(
+      body: Container(
+        child: Center(
+          child:Text("Hello World"),
+        ),
+      ),
+    ),
+  );
+  runApp(app);
+}
